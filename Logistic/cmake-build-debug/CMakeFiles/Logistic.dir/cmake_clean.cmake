4c4bb5c9d4327e3ca1343de411f4b0debe8a4e33
EprimePRO <emanuel_rios9@yahoo.com>
2020-02-10 20:16:03 -0600

    added the skeleton c++ projects.

---
+++ Logistic/cmake-build-debug/CMakeFiles/Logistic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Logistic.dir/main.cpp.obj"
+  "Logistic.pdb"
+  "Logistic.exe"
+  "Logistic.exe.manifest"
+  "libLogistic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Logistic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
