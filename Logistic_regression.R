00f03fbd9af1e6652689eef594bb0e3547a22dbc
MarianaCastRoiz <50093696+MarianaCastRoiz@users.noreply.github.com>
2020-02-10 22:07:11 -0600

    Add files via upload

---
+++ Logistic_regression.R
@@ -0,0 +1,30 @@
+library(HSAUR)
+data("plasma")
+df <- data.frame(plasma)
+set.seed(1234)
+glm1 <- glm(ESR~fibrinogen, family="binomial", data = df)
+summary(glm1)
+
+#linear regression from scrat
+sigmoid <- function(z){
+  1.0 / (1+exp(-z))
+}
+
+weights <- c(1,1)
+data_matrix <- cbind(rep(1, nrow(df)), df$fibrinogen)
+labels <- as.integer(df$ESR)
+
+#Gradient descent
+learning_rate <- 0.001
+for(i in 1:500000){
+  prob_vector <- sigmoid(data_matrix %*% weights)
+  error <- labels - prob_vector
+  weights <- weights + learning_rate * t(data_matrix) %*% error
+}
+weights
+
+#log odds
+plasma_log_odds <- cbind(rep(1,32), plasma$fibrinogen) %*% weights
+plot(plasma$fibrinogen, plasma_log_odds, col=plasma$ESR)
+abline(weights[1], weights[2])
+
